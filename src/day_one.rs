3c2f213f57da9918eca7da4bfbed925d34f93e86
Jeff Mattfield <grybyx@gmail.com>
2020-12-12 01:19:47 -0600

    answered day 1 part 2

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,31 +1,83 @@
 use std::{fs, io};
 
-fn find_twenty_twenty_sum(list: &Vec<u32>) -> Option<(u32, u32)> {
-    if let Some(a) = list.iter().find(|&&x| list.iter().any(|&y| x + y == 2020)) {
-        Some((*a, 2020 - *a))
-    } else {
-        None
-    }
+struct DayOne {
+    list: Vec<u32>,
 }
 
-fn load_list_from_file(filename: &str) -> Result<Vec<u32>, io::Error> {
-    let mut result: Vec<u32> = vec![];
-
-    let contents = fs::read_to_string(filename)?;
-
-    for line in contents.lines() {
-        println!("line: \"{}\"", line);
-        result.push(line.parse().unwrap());
+impl DayOne {
+    fn with_list(list: Vec<u32>) -> Self {
+        Self { list }
     }
 
-    Ok(result)
+    fn from_file(filename: &str) -> Self {
+        let list = Self::load_list_from_file(filename).unwrap();
+        Self { list }
+    }
+
+    fn find_pair(list: &[u32], sum: u32) -> Option<(u32, u32)> {
+        for x in list {
+            if *x > sum {
+                continue;
+            }
+
+            if list[1..].contains(&(sum - *x)) {
+                return Some((*x, sum - x));
+            }
+        }
+
+        None
+    }
+
+    fn find_triple(list: &[u32], sum: u32) -> Option<(u32, u32, u32)> {
+        for x in list {
+            if *x > sum {
+                continue;
+            }
+
+            if let Some((y, z)) = Self::find_pair(&list[1..], sum - *x) {
+                return Some((*x, y, z));
+            }
+        }
+
+        None
+    }
+
+    fn load_list_from_file(filename: &str) -> Result<Vec<u32>, io::Error> {
+        let mut result: Vec<u32> = vec![];
+
+        let contents = fs::read_to_string(filename)?;
+
+        for line in contents.lines() {
+            result.push(line.parse().unwrap());
+        }
+
+        Ok(result)
+    }
+
+    fn part_one(&self, sum: u32) {
+        println!("Part One\n========");
+        if let Some((a, b)) = Self::find_pair(&self.list, sum) {
+            println!("a: {}, b: {}, a*b: {}", a, b, a * b);
+        } else {
+            println!("No solution found for part one");
+        }
+    }
+
+    fn part_two(&self, sum: u32) {
+        println!("Part Two\n========");
+        if let Some((a, b, c)) = Self::find_triple(&self.list, sum) {
+            println!("a: {}, b: {}, c: {}, a*b*c: {}", a, b, c, a * b * c);
+        } else {
+            println!("No solution found for part two");
+        }
+    }
 }
 
 pub fn run(filename: &str) {
-    let list = load_list_from_file(filename).unwrap();
-    if let Some((a, b)) = find_twenty_twenty_sum(&list) {
-        println!("a: {}, b: {}, a*b: {}", a, b, a * b);
-    }
+    let day_one = DayOne::from_file(filename.into());
+
+    day_one.part_one(2020);
+    day_one.part_two(2020);
 }
 
 #[cfg(test)]
@@ -35,9 +87,19 @@ mod test {
     #[test]
     fn test_finds_correct_pair() {
         let list = vec![1721, 979, 366, 299, 675, 1456];
-        let (a, b) = find_twenty_twenty_sum(&list).unwrap();
+        let (a, b) = DayOne::find_pair(&list, 2020).unwrap();
 
         assert_eq!(a, 1721);
         assert_eq!(b, 299);
+    }
+
+    #[test]
+    fn test_finds_correct_triple() {
+        let list = vec![1721, 979, 366, 299, 675, 1456];
+        let (a, b, c) = DayOne::find_triple(&list, 2020).unwrap();
+
+        assert_eq!(a, 979);
+        assert_eq!(b, 366);
+        assert_eq!(c, 675);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
-use std::env;
+use std::fs;
+
+use clap::{App, Arg};
 
 mod day_one;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("Advent of Code 2020 Runner")
+        .version("0.1.0")
+        .author("Jeff Mattfield")
+        .about("Runs solutions to the problems posed by the 2020 edition of the Advent of Code (https://adventofcode.com/2020)")
+        .arg(
+            Arg::with_name("day")
+                .help("the day of the month")
+                .index(1)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("filename")
+                .help("the path to a file used by the day")
+                .short("f")
+                .required(false)
+                .takes_value(true),
+        )
+        .get_matches();
 
-    if args.len() < 3 {
-        panic!("parameters required: day file");
-    }
-
-    let day: &str = &args[1];
-    let filename: &str = &args[2];
+    let day = matches.value_of("day").unwrap_or("1");
 
     match day {
-        "one" => day_one::run(filename),
-        _ => unimplemented!(),
+        "1" => {
+            if let Some(filename) = matches.value_of("filename") {
+                day_one::run(filename);
+            } else {
+                println!("Day 1 requires an input file");
+            }
+        }
+
+        _ => println!("Day \"{}\" is not implemented or is not valid", day),
     }
 }
