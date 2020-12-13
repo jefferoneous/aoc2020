ab19836b86df5b9907ba7134bf48fe87885d8396
Jeff Mattfield <grybyx@gmail.com>
2020-12-13 03:28:52 -0600

    answer day 2 part 2

--- src/day_two.rs
+++ src/day_two.rs
@@ -5,8 +5,8 @@ use std::str::FromStr;
 
 #[derive(Debug)]
 struct PasswordSpec {
-    min_count: u8,
-    max_count: u8,
+    a: u8,
+    b: u8,
     character: char,
     password: String,
 }
@@ -14,8 +14,8 @@ struct PasswordSpec {
 #[derive(Debug, PartialEq, Eq)]
 enum ParseError {
     InvalidSyntax,
-    MinCountTooLow,
-    MaxCountLowerThanMinCount,
+    ATooLow,
+    BLowerThanA,
     MissingRequiredCharacterSpec,
 }
 
@@ -26,9 +26,8 @@ impl Display for ParseError {
             "{}",
             match self {
                 ParseError::InvalidSyntax => "Specification doesn't follow syntax",
-                ParseError::MinCountTooLow => "Minimum count can't be less than 1",
-                ParseError::MaxCountLowerThanMinCount =>
-                    "Maximum count can't be less than minimum count",
+                ParseError::ATooLow => "A can't be less than 1",
+                ParseError::BLowerThanA => "B can't be less than A",
                 ParseError::MissingRequiredCharacterSpec =>
                     "Required character specification is missing",
             }
@@ -46,18 +45,18 @@ impl FromStr for PasswordSpec {
             return Err(ParseError::InvalidSyntax);
         }
 
-        let min_count = parts[0].parse().unwrap_or_default();
-        let max_count = parts[1].parse().unwrap_or_default();
+        let a = parts[0].parse().unwrap_or_default();
+        let b = parts[1].parse().unwrap_or_default();
         let character = parts[2].chars().next().unwrap_or_default();
         // note we skip index 3 because it should be an empty string
         let password = parts[4].into();
 
-        if min_count < 1 {
-            return Err(ParseError::MinCountTooLow);
+        if a < 1 {
+            return Err(ParseError::ATooLow);
         }
 
-        if max_count < min_count {
-            return Err(ParseError::MaxCountLowerThanMinCount);
+        if b < a {
+            return Err(ParseError::BLowerThanA);
         }
 
         if character == char::default() {
@@ -65,8 +64,8 @@ impl FromStr for PasswordSpec {
         }
 
         Ok(Self {
-            min_count,
-            max_count,
+            a: a,
+            b: b,
             character,
             password,
         })
@@ -74,23 +73,62 @@ impl FromStr for PasswordSpec {
 }
 
 impl PasswordSpec {
-    fn has_valid_password(&self) -> bool {
+    fn has_valid_sled_password(&self) -> bool {
         let count = self
             .password
             .chars()
             .filter(|&c| c == self.character)
             .count() as u8;
-        count >= self.min_count && count <= self.max_count
+        count >= self.a && count <= self.b
+    }
+
+    fn has_valid_toboggan_password(&self) -> bool {
+        if ((self.a - 1) as usize) < self.password.len()
+            || ((self.b - 1) as usize) < self.password.len()
+        {
+            let first_char = self
+                .password
+                .chars()
+                .nth((self.a - 1) as usize)
+                .unwrap_or_default();
+            let second_char = self
+                .password
+                .chars()
+                .nth((self.b - 1) as usize)
+                .unwrap_or_default();
+
+            first_char != second_char
+                && (first_char == self.character || second_char == self.character)
+        } else {
+            false
+        }
     }
 }
 
-fn count_invalid_passwords(list: &Vec<String>) -> usize {
+fn count_invalid_passwords_sled_style(list: &Vec<String>) -> usize {
     let mut count = 0;
 
     for item in list {
         match item.parse::<PasswordSpec>() {
             Ok(spec) => {
-                if spec.has_valid_password() {
+                if spec.has_valid_sled_password() {
+                    count += 1;
+                }
+            }
+            Err(e) => eprintln!("Could not parse \"{}\": {}", item, e),
+        }
+    }
+
+    count
+}
+
+fn count_invalid_passwords_toboggan_style(list: &Vec<String>) -> usize {
+    let mut count = 0;
+
+    for item in list {
+        match item.parse::<PasswordSpec>() {
+            Ok(spec) => {
+                if spec.has_valid_toboggan_password() {
                     count += 1;
                 }
             }
@@ -103,18 +141,15 @@ fn count_invalid_passwords(list: &Vec<String>) -> usize {
 
 fn part_one(list: &Vec<String>) {
     println!("Part One\n========");
-    let count = count_invalid_passwords(list);
-    println!("Invalid passwords: {}", count);
+    let count = count_invalid_passwords_sled_style(list);
+    println!("Valid passwords: {}", count);
 }
 
-// fn part_two(list: &[u32], sum: u32) {
-//     println!("Part Two\n========");
-//     if let Some((a, b, c)) = find_triple(list, sum) {
-//         println!("a: {}, b: {}, c: {}, a*b*c: {}", a, b, c, a * b * c);
-//     } else {
-//         println!("No solution found for part two");
-//     }
-// }
+fn part_two(list: &Vec<String>) {
+    println!("Part Two\n========");
+    let count = count_invalid_passwords_toboggan_style(list);
+    println!("Valid passwords: {}", count);
+}
 
 fn load_list_from_file(filename: &str) -> Result<Vec<String>, IoError> {
     let input = File::open(filename)?;
@@ -128,7 +163,7 @@ pub fn run(filename: &str) {
     match load_list_from_file(filename) {
         Ok(list) => {
             part_one(&list);
-            // part_two(&list, 2020);
+            part_two(&list);
         }
         Err(e) => eprintln!("Error occurred while reading input file: {}", e),
     }
@@ -142,8 +177,8 @@ mod test {
     fn parses_valid_spec() {
         let spec: PasswordSpec = "1-3 a: abcde".parse().unwrap();
 
-        assert_eq!(spec.min_count, 1);
-        assert_eq!(spec.max_count, 3);
+        assert_eq!(spec.a, 1);
+        assert_eq!(spec.b, 3);
         assert_eq!(spec.character, 'a');
         assert_eq!(spec.password, "abcde");
     }
@@ -159,14 +194,14 @@ mod test {
     fn cannot_parse_spec_with_min_less_than_one() {
         let result = "0-4 a: abcde".parse::<PasswordSpec>().err();
 
-        assert_eq!(result, Some(ParseError::MinCountTooLow));
+        assert_eq!(result, Some(ParseError::ATooLow));
     }
 
     #[test]
     fn cannot_parse_spec_with_max_less_than_min() {
         let result = "4-1 a: abcde".parse::<PasswordSpec>().err();
 
-        assert_eq!(result, Some(ParseError::MaxCountLowerThanMinCount));
+        assert_eq!(result, Some(ParseError::BLowerThanA));
     }
 
     #[test]
@@ -177,16 +212,37 @@ mod test {
     }
 
     #[test]
-    fn identifies_valid_password() {
+    fn identifies_valid_sled_password() {
         let spec: PasswordSpec = "1-3 a: abcde".parse().unwrap();
 
-        assert!(spec.has_valid_password());
+        assert!(spec.has_valid_sled_password());
     }
 
     #[test]
-    fn identifies_invalid_password() {
+    fn identifies_invalid_sled_password() {
         let spec: PasswordSpec = "1-3 b: cdefg".parse().unwrap();
 
-        assert_eq!(false, spec.has_valid_password());
+        assert_eq!(false, spec.has_valid_sled_password());
+    }
+
+    #[test]
+    fn identifies_valid_toboggan_password() {
+        let spec: PasswordSpec = "1-3 a: abcde".parse().unwrap();
+
+        assert!(spec.has_valid_toboggan_password());
+    }
+
+    #[test]
+    fn identifies_invalid_toboggan_password_with_missing_char() {
+        let spec: PasswordSpec = "1-3 b: cdefg".parse().unwrap();
+
+        assert_eq!(false, spec.has_valid_toboggan_password());
+    }
+
+    #[test]
+    fn identifies_invalid_toboggan_password_with_repeat_char() {
+        let spec: PasswordSpec = "2-9 c: ccccccccc".parse().unwrap();
+
+        assert_eq!(false, spec.has_valid_toboggan_password());
     }
 }
